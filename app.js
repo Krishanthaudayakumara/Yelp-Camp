d9ac9ae0e20e1997a0043ac16d7cf0df16565bce
Krishantha <udayakumarakrishantha@gmail.com>
2021-07-09 18:48:25 +0530

    Till Add new function

--- app.js
+++ app.js
@@ -17,21 +17,37 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
-})
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'))
 
+app.use(express.urlencoded({ extended: true}))
+
 app.get('/', (req, res) => {
   res.render('home')
+});
+
+app.get('/campgrounds', async (req,res) => {
+    const Campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { Campgrounds })
+});
+
+app.get('/campgrounds/new',(req,res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async(req, res) =>{
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
-app.get('/makecampground', async (req,res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'nice camping' });
-    await camp.save();
-    res.send(camp)
-})
+app.get('/campgrounds/:id', async (req,res) => {
+  const campground = await Campground.findById(req.params.id)
+  res.render('campgrounds/show',{ campground });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
