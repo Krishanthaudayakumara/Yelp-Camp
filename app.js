ad9b372c665f6d330657345f3053802dbb818fe5
Krishantha <udayakumarakrishantha@gmail.com>
2021-07-10 14:12:26 +0530

    Added CRUD function- Basic
    CRUD+ Layout basic

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+
+
+const port = 3000;
+const app = express();
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useNewUrlParser: true,
@@ -19,10 +23,16 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.engine('ejs',ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'))
 
+//*********************************************************************/
+
 app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
+
+//*********************************************** */
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -44,9 +54,26 @@ app.post('/campgrounds', async(req, res) =>{
 })
 
 app.get('/campgrounds/:id', async (req,res) => {
-  const campground = await Campground.findById(req.params.id)
+  const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show',{ campground });
 });
+
+app.get('/campgrounds/:id/edit', async (req,res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async(req,res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async(req,res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
